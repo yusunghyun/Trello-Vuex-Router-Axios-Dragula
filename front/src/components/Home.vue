ce73642fa8657e437d1c6cebee2d06f349835618
SungHyun Yu <ysh1513@naver.com>
2020-01-01 15:05:55 +0900

    axios

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -3,6 +3,12 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <div v-else>
+        <div v-for=" b in boards " :key="b.id">
+          {{b}}
+        </div>
+      </div>
         <ul>
           <li>
             <router-link to='/b/1'>Board1</router-link>
@@ -16,8 +22,30 @@
 </template>
 
 <script>
+import {board} from '../api'
+
 export default {
-  
+  data(){
+    return {
+      loading:false,
+      boards:'',
+    }
+  },
+  created(){
+    this.fetchData()
+  },
+  methods:{
+    fetchData(){
+      this.loading=true
+      board.fetch()
+        .then(data=>{
+          this.boards = data
+        })
+        .finally( ()=>{
+          this.loading=false
+        })
+    }
+  }
 }
 </script>
 
