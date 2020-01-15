34dcd85c91bf5c16da12f8aa192c2fcf574d6b27
SungHyun Yu <ysh1513@naver.com>
2020-01-15 14:42:56 +0900

    dragula refactoring

--- front/src/components/Board.vue
+++ front/src/components/Board.vue
@@ -21,8 +21,8 @@
 <script>
 import {mapState, mapActions} from 'vuex'
 import List from './List.vue'
-import dragula from 'dragula'
-import 'dragula/dist/dragula.css'
+import dragger from '../utills/dragger'
+
 
 export default {
   components: { List },
@@ -30,7 +30,7 @@ export default {
     return {
       bid: 0,
       loading: false,
-      dragulaCards:null,
+      cDragger:null,
     }
   },
   computed: {
@@ -42,37 +42,9 @@ export default {
     this.fetchData()
   },
   updated(){//자식컴포가 마운트된 시점
-    if(this.dragulaCards) this.dragulaCards.destroy()
+    this.setCardDragabble()
     //daragula(container,option)
-    this.dragulaCards = dragula([
-      ...Array.from(this.$el.querySelectorAll('.card-list'))
-    ]).on('drop',(el,wrapper,target,siblings)=>{
-      const targetCard = {
-        id:el.dataset.cardId * 1,
-        pos: 65535
-      }
-      let prevCard = null
-      let nextCard = null
-      Array.from(wrapper.querySelectorAll('.card-item'))
-        .forEach((el,idx,arr)=>{
-          const cardId = el.dataset.cardId * 1
-          if(cardId == targetCard.id) {
-            prevCard = idx > 0 ? {
-              id: arr[idx -1 ].dataset.cardId * 1,
-              pos: arr[idx -1 ].dataset.cardPos * 1,
-            } : null;
-            nextCard = idx < arr.length -1 ?{
-              id:arr[idx+1].dataset.cardId * 1,
-              pos:arr[idx+1].dataset.cardPos * 1
-            } : null;
-          }
-        })
-      if(!prevCard && nextCard) targetCard.pos = nextCard.pos / 2
-      else if (!nextCard && prevCard) targetCard.pos = prevCard.pos * 2
-      else if (prevCard && nextCard) targetCard.pos = (prevCard.pos + nextCard.pos)/2
-      console.log(targetCard)
-      this.UPDATE_CARD(targetCard)//이게 api 넘기기 간단!
-  })
+    
   },
   methods: {
     ...mapActions([
@@ -83,6 +55,28 @@ export default {
       this.loading = true
       this.FETCH_BOARD({id: this.$route.params.bid})
         .then(() => this.loading = false)
+    },
+    setCardDragabble(){
+      if(this.cDragger) this.cDragger.destroy()
+      this.cDragger = dragger.init(Array.from(this.$el.querySelectorAll('.card-list')))
+      this.cDragger.on('drop',(el,wrapper,target,siblings)=>{
+      const targetCard = {
+        id:el.dataset.cardId * 1,
+        pos: 65535
+      }
+      const {prev, next} = dragger.sibling({
+        el,
+        wrapper,
+        candidates:Array.from(wrapper.querySelectorAll('.card-item')),
+        type:'card'
+      })
+      if(!prev && next) targetCard.pos = next.pos / 2
+      else if (!next && prev) targetCard.pos = prev.pos * 2
+      else if (prev && next) targetCard.pos = (prev.pos + next.pos)/2
+      console.log(targetCard)
+      this.UPDATE_CARD(targetCard)//이게 api 넘기기 간단!
+      
+      })
     }
   }
 }
