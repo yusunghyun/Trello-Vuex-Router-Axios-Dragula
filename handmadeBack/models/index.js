7a64e46861274239de28bef2003c2af2d01f39a5
SungHyun Yu <ysh1513@naver.com>
2020-02-22 02:12:18 +0900

    AWS

--- handmadeBack/models/index.js
+++ handmadeBack/models/index.js
@@ -23,11 +23,8 @@ db.Board=require('./Board.js')(sequelize,Sequelize);
 db.Card=require('./Card.js')(sequelize,Sequelize);
 db.List=require('./List.js')(sequelize,Sequelize);
 
-db.Board.belongsTo(db.User,{onUpdate: 'CASCADE',
-onDelete: 'CASCADE',})
-db.Board.hasMany(db.List,{onUpdate: 'CASCADE',
-onDelete: 'CASCADE'})
-db.List.hasMany(db.Card,{onUpdate: 'CASCADE',
-onDelete: 'CASCADE',})
+// db.Board.belongsTo(db.User)
+db.Board.hasMany(db.List)
+db.List.hasMany(db.Card)
 
 module.exports = db;
